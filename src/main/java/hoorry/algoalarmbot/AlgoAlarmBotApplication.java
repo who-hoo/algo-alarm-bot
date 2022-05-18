7ca7d3f62e14cbd9a52e07312c5c3c2fe29880ba
hsjang <hsjang0405@gmail.com>
2022-05-19 02:18:19 +0900

    ✨  /money 전체 공개하도록 변경

--- src/main/java/hoorry/algoalarmbot/AlgoAlarmBotApplication.java
+++ src/main/java/hoorry/algoalarmbot/AlgoAlarmBotApplication.java
@@ -1,5 +1,6 @@
 package hoorry.algoalarmbot;
 
+import com.slack.api.app_backend.slash_commands.response.SlashCommandResponse;
 import com.slack.api.bolt.App;
 import com.slack.api.bolt.jetty.SlackAppServer;
 import hoorry.algoalarmbot.money.MoneyService;
@@ -11,10 +12,8 @@ public class AlgoAlarmBotApplication {
         App app = new App();
         MoneyService moneyService = new MoneyService();
 
-        // TEST COMMAND
-        app.command("/hello", (req, ctx) -> {
-            String arg = req.getPayload().getText();
-            return ctx.ack(req.getPayload().getText());
+        app.command("/recommend", (req, ctx) -> {
+	        return ctx.ack();
         });
 
         app.command("/money", (req, ctx) -> {
@@ -30,7 +29,10 @@ public class AlgoAlarmBotApplication {
                 },
                 () -> message.set("유효하지 않은 명령어입니다.")
             );
-            return ctx.ack(message.toString());
+	        return ctx.ack(SlashCommandResponse.builder()
+		        .text(message.toString())
+		        .responseType("in_channel")
+		        .build());
         });
 
         SlackAppServer server = new SlackAppServer(app);
